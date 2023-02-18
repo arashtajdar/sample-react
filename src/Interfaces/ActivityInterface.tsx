5b81779707a17b91d7203026f72af0f86e272b1d
arashtajdar <arash.tajdar@gmail.com>
2023-02-18 12:15:25 +0100

    Add activity list in home page

---
+++ src/Interfaces/ActivityInterface.tsx
@@ -0,0 +1,13 @@
+export interface ActivityInterface {
+    "Activity_id": Number,
+    "Activity_code": String,
+    "Activity_title": String,
+    "Activity_description": String,
+    "Activity_type": String,
+    "Activity_user": String,
+    "Activity_city": String,
+    "Activity_location": String,
+    "Activity_start": String,
+    "Activity_end": String,
+    "created_at": String
+}
--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -1,28 +1,28 @@
-import {throws} from "assert";
+import {ActivityInterface} from "../Interfaces/ActivityInterface";
+import {useEffect, useState} from "react";
 
 function Activities() {
-    let keys;
-    keys = [
-        {'name' : 'ACT001'},
-        {'name' : 'ACT002'},
-        {'name' : 'ACT003'},
-        {'name' : 'ACT004'},
-        {'name' : 'ACT005'},
-        {'name' : 'ACT006'},
-        {'name' : 'ACT007'},
-        {'name' : 'ACT008'},
-    ];
+    const [Activities, setActivities] = useState(Array<ActivityInterface>);
+
+
+    // useEffect(()=>{
+        fetch("http://127.0.0.1:8000/api/Activities")
+            .then((response) => response.json())
+            .then((data) => {
+                setActivities(data);
+            });
+    // });
     return (
         <div className="container">
             <div className="div content">
                 <div className="categories">
-                {keys.map((item) => (
+                {Activities.map((item) => (
                     <div className="categories_item">
                         <a href="" className="">
                             <span><img className="activity-thumbnail" src="https://media.licdn.com/dms/image/sync/D5618AQEuOiIsSCZUHw/companyUpdate-article-image-shrink_627_1200/0/1676589899160/Deploy1png?e=1679529600&v=beta&t=VzhBOmFxls-W-CmB662L4BCpynZ2f2PQyuznMmoTxus"></img></span><br></br>
-                            <span>Name : {item.name}</span><br></br>
-                            <span>City : {item.name}</span><br></br>
-                            <span>Start : {item.name}</span> - <span>End : {item.name}</span><br></br>
+                            <span>Name : {item.Activity_title}</span><br></br>
+                            <span>City : {item.Activity_city}</span><br></br>
+                            <span>Start : {item.Activity_start}</span> - <span>End : {item.Activity_end}</span><br></br>
                         </a>
                     </div>
                 ))}
